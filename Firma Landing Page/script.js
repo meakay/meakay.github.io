ec4334e9544af621ab9e5393ece7012cc1852a28
meakay <77950557+meakay@users.noreply.github.com>
2023-11-05 12:56:46 +0300

    Update script.js

--- Firma Landing Page/script.js
+++ Firma Landing Page/script.js
@@ -1,3 +1,5 @@
-$(document).ready(function(){
-    $(".preloader").fadeOut();
-    })
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.querySelector("body").classList.add("loaded");
+    }, 10)
+  });
